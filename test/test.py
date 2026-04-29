cde76a4728d03db9fe4b0c27e706f3d61e4cd47a
Terrence Jefferson Cimafranca <40508730+tjcmfrnc@users.noreply.github.com>
2026-04-29 17:26:25 +0800

    Update clock unit and test input values

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -26,8 +26,8 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    dut.ui_in.value = 20
+    dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
